8fbd30a9386b4ea2bb9067459b1e3cefe6658f49
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2023-02-19 22:24:44 +0200

    fix App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,14 +13,24 @@ export const App =()=>{
   const [filteredData, setFilteredData] = useState([]);
 
   useEffect(()=>{
-
-if (!localStorage.getItem('contacts')) {
-  localStorage.setItem('contacts', JSON.stringify(contacts));
-}  
-if (!!JSON.parse(localStorage.getItem('contacts')).length) {
-  setContacts([...JSON.parse(localStorage.getItem("contacts"))]);
-}
+    startDidMount ();
+// if (!localStorage.getItem('contacts')) {
+//   localStorage.setItem('contacts', JSON.stringify(contacts));
+// }  
+// if (!!JSON.parse(localStorage.getItem('contacts')).length) {
+//   setContacts([...JSON.parse(localStorage.getItem("contacts"))]);
+// }
+// eslint-disable-next-line react-hooks/exhaustive-deps
 }, [])
+
+const startDidMount = () =>{
+  if (!localStorage.getItem('contacts')) {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }  
+  if (!!JSON.parse(localStorage.getItem('contacts')).length) {
+    setContacts([...JSON.parse(localStorage.getItem("contacts"))]);
+  }
+}
 
 useEffect(()=>{
    localStorage.setItem('contacts', JSON.stringify(contacts));
